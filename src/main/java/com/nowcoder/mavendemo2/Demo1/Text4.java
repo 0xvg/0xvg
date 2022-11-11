d487b91e2898be7a81b4e1526fe7311833231bd9
有点废 <2053414661@qq.com>
2022-11-12 00:23:38 +0800

    尝试2

---
+++ src/main/java/com/nowcoder/mavendemo2/Demo1/Text4.java
@@ -0,0 +1,8 @@
+package com.nowcoder.mavendemo2.Demo1;
+
+public class Text4 {
+    public static void main(String[] args) {
+        int[][] arr={{1,4},{3,5},{0,6},{4,7},{3,9},{5,9},{6,10},{8,11},{8,12},{2,14},{12,16}};
+
+    }
+}
