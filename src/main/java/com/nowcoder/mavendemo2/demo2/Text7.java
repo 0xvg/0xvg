6f90c9114e3f7928b956b4dc47cc924fec4ad8d8
有点废 <2053414661@qq.com>
2022-11-12 16:03:13 +0800

    更新

---
+++ src/main/java/com/nowcoder/mavendemo2/demo2/Text7.java
@@ -0,0 +1,11 @@
+package com.nowcoder.mavendemo2.demo2;
+
+/**
+ * @Auther:xvg
+ * @Date:2022/11/12 - 11 - 12 - 16:02
+ */
+public class Text7 {
+    public static void main(String[] args) {
+        System.out.println("有点毛病");
+    }
+}
